74c54f587da8eecf7c630bc50fbb0e1fac827a11
Blake Raymond <blake.crypto@technologist.com>
2023-04-14 13:25:42 -0600

    update

--- gitInfo.js
+++ gitInfo.js
@@ -11,6 +11,8 @@
 
 //CODE HERE
 
+let gitDefiniton = "git is a version control that allows you to save a repository of code on your local machine. It allows you to see what changes have been made over time, like a 'code journal'."
+
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,6 +20,8 @@
 */
 
 //CODE HERE
+
+let gitHubDefinition = "gitHub is an online version of your repository, or a cloud version. It does the same things as git, but allows collaboration and access from anywhere with an internet connection to your projects."
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -27,6 +31,8 @@
 
 //CODE HERE
 
+let gitInitDefinition = "Git Init is what you type into the bash/zsh shell to initialize your 'git' or repository. It creates an empty 'git' file that is where your repository will be stored."
+
 //////////////////PROBLEM 4////////////////////
 /*
     Create a variable called 'gitCloneDefinition'.  
@@ -35,6 +41,8 @@
 
 //CODE HERE
 
+let gitCloneDefinition = "git clone is your command that you type into the bash shell that allows you to make a copy of a repository from github onto your local machine."
+
 //////////////////PROBLEM 5////////////////////
 /*
     Create a variable called 'gitStatusDefinition'.  
@@ -42,6 +50,8 @@
 */
 
 //CODE HERE
+
+let gitStatusDefinition = "git status is a command you type into your bash shell that allows you to see the status of your files, for instance whether code has been staged/saved, uploaded, and stored on gitHub"
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -54,6 +64,9 @@
 
 //CODE HERE
 
+let gitAddDefinition = "the gitAddDefinition function allows you to save your code in it's current state."
+let gitAddCode = "git add ." //('.' can also be a fileName)
+
 //////////////////PROBLEM 7////////////////////
 /*
     Create a variable called 'gitCommitDefinition'.  
@@ -65,6 +78,8 @@
 
 //CODE HERE
 
+const gitCommitDefinition = "git Commit function allows you to save your work permanently to your 'git' file. It is no longer removable. Once committed, it is ready for upload."
+const getCommitCode = "git commit -m ' '" //where "m" commands 'message' to be placed between "' '"
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,3 +88,5 @@
 */
 
 //CODE HERE
+
+let gitPushDefinition = "This is the final step. This is where we push/upload our local repository to gitHub for online access."
